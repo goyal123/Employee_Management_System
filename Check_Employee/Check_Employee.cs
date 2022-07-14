3184875094cf58c3f70138014797769524090d65
goyal123 <akhilg642@gmail.com>
2022-07-14 14:22:29 +0530

    Employee Check

---
+++ Check_Employee/Check_Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emp_Manage_Sys
+{
+    public class Check_Employee
+    {
+        public static void Emp_check()
+        { 
+            Random random = new Random();
+            int emp_check = random.Next(0, 2);
+            if(emp_check==0)
+            {
+                Console.WriteLine("Employee is absent");
+            }
+            else
+            {
+                Console.WriteLine("Employee is present");
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Management System");
+            Check_Employee.Emp_check();
         }
     }
 }
