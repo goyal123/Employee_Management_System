060835f3507a66524205708352c3f0063429c111
goyal123 <akhilg642@gmail.com>
2022-07-14 17:09:55 +0530

    Adding condition of Max working Days=20 or Max working Hours=160 in a month

--- Check_Employee_Wage/Check_Employee_Wage.cs
+++ Check_Employee_Wage/Check_Employee_Wage.cs
@@ -16,7 +16,7 @@ namespace Emp_Manage_Sys
         {
             int emp_workHrs = 0, emp_Rate_Per_Hour = 20, emp_wage = 0;
             int FullTime = 1;
-            int emp_Time;
+            int emp_Time,work_Day=0;
             Random random = new Random();
             for (int day = 1; day <= 30; day++)
             {
@@ -26,11 +26,13 @@ namespace Emp_Manage_Sys
                     if (emp_Time == FullTime)
                     {
                         emp_workHrs = emp_workHrs + 8;
+                        work_Day++;
                         //Console.WriteLine("Employee is Present Full-Day");
                     }
                     else
                     {
                         emp_workHrs = emp_workHrs + 4;
+                        work_Day++;
                         //Console.WriteLine("Employee is Present Half-Day");
                     }
 
@@ -38,12 +40,19 @@ namespace Emp_Manage_Sys
                 else
                 {
                     emp_workHrs = emp_workHrs + 0;
+                    work_Day++;
                     //Console.WriteLine("Employee is Absent");
                 }
 
+                if (emp_workHrs > 160 || work_Day>20)
+                    break;
+
+                //emp_wage = emp_workHrs * emp_Rate_Per_Hour;
+                //Console.WriteLine("Employee wage at Day {0} = {1} for {2} working hours ",work_Day, emp_wage,emp_workHrs);
+
             }
             emp_wage = emp_workHrs * emp_Rate_Per_Hour;
-            Console.WriteLine("Employee wage = {0} ", emp_wage);
+            Console.WriteLine("Total Employee wages of month = {0} ", emp_wage);
         }
     }
 }
