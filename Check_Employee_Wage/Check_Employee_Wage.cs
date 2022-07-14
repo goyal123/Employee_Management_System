7d26a16bd8ae958796af3770838bbb2747cbe53b
goyal123 <akhilg642@gmail.com>
2022-07-14 16:49:37 +0530

    Calculating Total wages of a month

--- Check_Employee_Wage/Check_Employee_Wage.cs
+++ Check_Employee_Wage/Check_Employee_Wage.cs
@@ -6,39 +6,42 @@ using System.Threading.Tasks;
 
 namespace Emp_Manage_Sys
 {
-    public class Check_Employee_Wage:Check_Employee
+    public class Check_Employee_Wage : Check_Employee
     {
         public Check_Employee_Wage()
         {
             Console.WriteLine("Please find Employee wage calculation");
         }
         public void empWage()
-        { int emp_workHrs = 0, emp_Rate_Per_Hour = 20, emp_wage = 0;
-            int FullTime = 1, PartTime = 0;
+        {
+            int emp_workHrs = 0, emp_Rate_Per_Hour = 20, emp_wage = 0;
+            int FullTime = 1;
             int emp_Time;
-           Random random=new Random();
-            emp_Time=random.Next(0, 2);
-
-            if (Emp_check())
-            {   
-                if(emp_Time==FullTime)
+            Random random = new Random();
+            for (int day = 1; day <= 30; day++)
+            {
+                emp_Time = random.Next(0, 2);
+                if (Emp_check())
                 {
-                    emp_workHrs = 8;
-                    Console.WriteLine("Employee is Present Full-Day");
+                    if (emp_Time == FullTime)
+                    {
+                        emp_workHrs = emp_workHrs + 8;
+                        //Console.WriteLine("Employee is Present Full-Day");
+                    }
+                    else
+                    {
+                        emp_workHrs = emp_workHrs + 4;
+                        //Console.WriteLine("Employee is Present Half-Day");
+                    }
+
                 }
                 else
                 {
-                    emp_workHrs = 4;
-                    Console.WriteLine("Employee is Present Half-Day");
+                    emp_workHrs = emp_workHrs + 0;
+                    //Console.WriteLine("Employee is Absent");
                 }
-                
-            }
-            else
-            {
-                emp_workHrs = 0;
-                Console.WriteLine("Employee is Absent");
-            }
 
+            }
             emp_wage = emp_workHrs * emp_Rate_Per_Hour;
             Console.WriteLine("Employee wage = {0} ", emp_wage);
         }
