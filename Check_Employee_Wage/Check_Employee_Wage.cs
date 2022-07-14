3f84706cac4a5c64d22f9697d686751a0754abfc
goyal123 <akhilg642@gmail.com>
2022-07-14 16:06:47 +0530

    Adding Wage Calculation of Employee

---
+++ Check_Employee_Wage/Check_Employee_Wage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emp_Manage_Sys
+{
+    public class Check_Employee_Wage:Check_Employee
+    {
+        public Check_Employee_Wage()
+        {
+            Console.WriteLine("Please find Employee wage calculation");
+        }
+        public void empWage()
+        { int emp_workHrs = 0, emp_Rate_Per_Hour = 20, emp_wage = 0;
+
+            if (Emp_check())
+            {
+                emp_workHrs = 8;
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                emp_workHrs = 0;
+                Console.WriteLine("Employee is Absent");
+            }
+
+            emp_wage = emp_workHrs * emp_Rate_Per_Hour;
+            Console.WriteLine("Employee wage = {0} ", emp_wage);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 ﻿namespace Emp_Manage_Sys
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Management System");
-            Check_Employee.Emp_check();
+            //Check_Employee.Emp_check();
+            Check_Employee_Wage wage = new Check_Employee_Wage();
+            wage.empWage();
+
+
+
            
 
         }
