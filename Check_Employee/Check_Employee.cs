6d726ac7b3e2fa5ac7dbc6c102f1f3b25f220834
goyal123 <akhilg642@gmail.com>
2022-07-14 15:59:34 +0530

    Modifying Emp_check class and inherit it into Emp_wage class

--- Check_Employee/Check_Employee.cs
+++ Check_Employee/Check_Employee.cs
@@ -8,13 +8,35 @@ namespace Emp_Manage_Sys
 {
     public class Check_Employee
     {
-        public static void Emp_check()
-        {  
+        public static bool Emp_check()
+        {
+            int Present = 1;
             Random random = new Random();
             int emp_check = random.Next(0, 2);
-            int Present = 1,emp_workHrs=0,emp_Rate_Per_Hour=20,emp_wage=0;
+            if (emp_check == Present)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+                
+        }
 
-            if(emp_check==Present)
+    }
+
+
+    public class Check_Employee_Wage:Check_Employee
+    {
+        public Check_Employee_Wage()
+        {
+            Console.WriteLine("Please find Employee wage calculation");
+        }
+        public void empWage()
+        { int emp_workHrs = 0, emp_Rate_Per_Hour = 20, emp_wage = 0;
+
+            if (Emp_check())
             {
                 emp_workHrs = 8;
                 Console.WriteLine("Employee is Present");
@@ -28,6 +50,5 @@ namespace Emp_Manage_Sys
             emp_wage = emp_workHrs * emp_Rate_Per_Hour;
             Console.WriteLine("Employee wage = {0} ", emp_wage);
         }
-
     }
 }
